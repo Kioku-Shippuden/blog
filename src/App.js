d2e50dc7fafbe00ebafde8404b529ea99c5152a6
Kioku <anhkhoakioku@gmail.com>
2023-07-22 17:17:44 +0700

    Add React Router and React Router Dom Into Project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from '../src/pages/HomePage/HomePage';
+import ReadPage from '../src/pages/ReadPage/ReadPage';
+import WritePage from '../src/pages/WritePage/WritePage';
+import SignInPage from '../src/pages/SignInPage/SignInPage';
+import SignUpPage from '../src/pages/SignUpPage/SignUpPage';
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path='/home/:user_id' element={<HomePage />}></Route>
+        <Route path='/:user_id/:content_id' element={<ReadPage />}></Route>
+        <Route path='/newpost/:user_id' element={<WritePage />}></Route>
+        <Route path='/sign_in' element={<SignInPage />}></Route>
+        <Route path='/sign_up' element={<SignUpPage />}></Route>
+      </Routes>
     </div>
   );
 }
