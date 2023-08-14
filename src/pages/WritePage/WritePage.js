baf98fe16587056530fd5f38c86f4ba376f30b24
Kioku <anhkhoakioku@gmail.com>
2023-08-14 09:53:36 +0700

    Restructuring the directory

--- src/pages/WritePage/WritePage.js
+++ src/pages/WritePage/WritePage.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import '../../components/NavigatorComponent/Navigator';
+import '../../components/WriteContentComponent/WriteContent'
 
 function WritePage() {
   return (
