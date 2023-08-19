92cf536d84ba87ecc2a07b5b29e6d3c15999234c
Kioku <anhkhoakioku@gmail.com>
2023-08-19 15:44:19 +0700

    Add Logic Handle Sign Up

--- src/components/SignUpComponent/SignUpByEmail.js
+++ src/components/SignUpComponent/SignUpByEmail.js
@@ -1,8 +1,85 @@
-import React from 'react'
+import React, { useState, useRef } from 'react';
+import validator from 'validator';
+import { useNavigate } from 'react-router';
+import { callPostApiWithoutToken } from '../../helpers/request';
 import './style/SignUpByEmail.scss';
 
 function SignUpByEmail(props) {
   const {displayAllOption} = props;
+
+  const emailRef = useRef(null);
+  const userNameRef = useRef(null);
+  const passwordRef = useRef(null);
+  const confirmPassRef = useRef(null);
+  const dateOfBirthRef = useRef(null);
+
+  const navigate = useNavigate();
+  const [displayError, setDisplayError] = useState({
+		email: false,
+    userName: false,
+		password: false,
+    confirmPass: false,
+    dateOfBirth: false
+	});
+
+  const validationForm = (email, userName, password, confirmPass, dateOfBirth) => {
+		const validated = {
+      email: !validator.isEmail(email),
+      userName: userName.length <= 0,
+      password: password.length <= 0,
+      confirmPass: confirmPass !== password,
+      dateOfBirth: dateOfBirth.length <= 0
+    };
+  
+    return validated;
+	}
+
+  const onSubmit = async () => {
+    const formData = {
+      email: emailRef.current.value,
+      userName: userNameRef.current.value,
+      password: passwordRef.current.value,
+      confirmPass: confirmPassRef.current.value,
+      dateOfBirth: dateOfBirthRef.current.value
+    };
+
+    const validated = validationForm(
+      formData.email, 
+      formData.userName,
+      formData.password,
+      formData.confirmPass,
+      formData.dateOfBirth
+    );
+
+    if (Object.values(validationErrors).some(error => error)) {
+      setDisplayError(validated)
+			return;
+    }
+
+    try {
+      const apiUrl = 'http://localhost:3000/v1/api/auth/signup';
+      await callPostApiWithoutToken(apiUrl, {
+        "username": userNameRef.current.value,
+        "email"   : emailRef.current.value,
+        "password": passwordRef.current.value,
+        "birth"   : dateOfBirthRef.current.value
+      });
+
+      navigate('/sign_in');
+    } catch (err) {
+      throw(err)
+    } finally {
+    setDisplayError({
+      email: false,
+      userName: false,
+      password: false,
+      confirmPass: false,
+      dateOfBirth: false
+    });
+  }
+
+  }
+
   return (
     <div className='sign-up-email'>
       <div className='header'>Sign up with email</div>
@@ -13,36 +90,56 @@ function SignUpByEmail(props) {
       <div className='infor-box'>
         <div className='box-element'>
           <div className='title'>Email:</div>
-          <div className='input'>
-            <input type="text" name="name" />
+          <div className={displayError.email ? 'err-border ' : 'input'}>
+            <input ref={emailRef}/>
           </div>
         </div>
+        {
+					displayError.email &&
+					<div className='err'>Please enter a valid email address.</div>	
+				}
         <div className='box-element'>
           <div className='title'>Username:</div>
-          <div className='input'>
-            <input type="text" name="name" />
+          <div className={displayError.userName ? 'err-border ' : 'input'}>
+            <input ref={userNameRef}/>
           </div>
         </div>
+        {
+					displayError.userName &&
+					<div className='err'>Please enter a valid UserName.</div>	
+				}
         <div className='box-element'>
           <div className='title'>Date of births:</div>
-          <div className='input'>
-            <input type="text" name="name" />
+          <div className={displayError.dateOfBirth ? 'err-border ' : 'input'}>
+            <input type="date" ref={dateOfBirthRef}/>
           </div>
         </div>
+        {
+					displayError.dateOfBirth &&
+					<div className='err'>Please Select your date of birth.</div>	
+				}
         <div className='box-element'>
           <div className='title'>Password:</div>
-          <div className='input'>
-            <input type="text" name="name" />
+          <div className={displayError.password ? 'err-border ' : 'input'}>
+            <input ref={passwordRef}/>
           </div>
         </div>
+        {
+					displayError.password &&
+					<div className='err'>Please enter a valid Password.</div>	
+				}
         <div className='box-element'>
           <div className='title'>Confirm:</div>
-          <div className='input'>
-            <input type="text" name="name" />
+          <div className={displayError.confirmPass ? 'err-border ' : 'input'}>
+            <input ref={confirmPassRef}/>
           </div>
         </div>
+        {
+					displayError.confirmPass &&
+					<div className='err'>Password does not match.</div>	
+				}
       </div>
-      <div className='sign-in-btn'>Sign up</div>
+      <div className='sign-in-btn' onClick={onSubmit}>Sign up</div>
       <div className='footer' onClick={displayAllOption}>&lt; All sign up options</div>
     </div>
   )
