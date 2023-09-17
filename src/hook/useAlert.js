b800e0418c3db838f8ad77d724b05e6e4658ca9f
Kioku <anhkhoakioku@gmail.com>
2023-09-17 10:16:50 +0700

    Update Flow Handle Socket Event

--- src/hook/useAlert.js
+++ src/hook/useAlert.js
@@ -1,5 +1,9 @@
 import React, { createContext, useContext, useEffect, useState } from 'react';
+import { io } from 'socket.io-client';
+import { useSnackbar } from 'notistack';
+import useUserProfile from './useUserProfile';
 import { useAuth } from './useAuthentication';
+import SnackbarContent from './useSnackbarContent';
 import { callGetApiWithoutToken } from '../helpers/request';
 
 const AlertContext = createContext();
@@ -10,9 +14,58 @@ export function useAlert() {
 
 export function AlertProvider({ children }) {
   const { isAuthen } = useAuth();
+  const { enqueueSnackbar } = useSnackbar();
   const [alertState, setAlertState] = useState(0);
   const [notificationState, setNotificationState] = useState([]);
+  
+  const userProfile = useUserProfile();
+  const userId = userProfile?.userId;
 
+  // Handle Connect Socket
+  const socket = io.connect('http://localhost:3002', {
+    transports: ['websocket'],
+    query: { userId }
+  });
+
+  socket.on('connect', () => {
+    console.log('Connected to WebSocket server');
+  });
+
+  socket.on('friendRequest', (data) => {
+    handleFriendNotification(data);
+  });
+
+  socket.on('acceptedRequest', (data) => {
+    handleFriendNotification(data);
+  })
+
+  socket.on('connect_error', (error) => {
+    console.error('Connection error:', error);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Disconnected from WebSocket server');
+  });
+
+  // Handle Notification About Friend
+  const handleFriendNotification = (data) => {
+    setAlertState((prev) => prev + 1);
+
+    const updatedNotification = [data, ...notificationState];
+    setNotificationState(updatedNotification);
+
+    enqueueSnackbar(
+      <SnackbarContent message={data.notifyMessage} />, 
+      {
+        style: {
+          backgroundColor: '#ffffff',
+          borderRadius: '25px',
+        },
+      }
+    );
+  }
+
+  // Handle Get All Notification
   const getAllNotification = async () => {
     if (!isAuthen) return;
     
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,66 +1,14 @@
-import io from 'socket.io-client';
-import React, {useState} from 'react'
-import { useSnackbar } from 'notistack';
-import { useAlert } from '../../hook/useAlert';
-import useUserProfile from '../../hook/useUserProfile';
+import React, {useState} from 'react';
 import Body from '../../components/BodyComponent/Body';
-import SnackbarContent from '../../hook/useSnackbarContent';
 import Navigator from '../../components/NavigatorComponent/Navigator';
 import ProfilePopup from '../../components/NavigatorComponent/ProfilePopup';
 import NotificationPopup from '../../components/NavigatorComponent/NotificationPopup';
 import './HomePage.scss';
 
 function HomePage() {
-  const { enqueueSnackbar } = useSnackbar();
-  const { setAlertState, setNotificationState, notificationState } = useAlert();
   const [showProfilePopup, setShowProfilePopup] = useState(false);
   const [showNotificationPopup, setShowNotificationPopup] = useState(false);
 
-  const userProfile = useUserProfile();
-  const userId = userProfile?.userId;
-
-  const handleFriendNotification = (data) => {
-    setAlertState((prev) => prev + 1);
-
-    const updatedNotification = [data, ...notificationState];
-    setNotificationState(updatedNotification);
-
-    enqueueSnackbar(
-      <SnackbarContent message={data.notifyMessage} />, 
-      {
-        style: {
-          backgroundColor: '#ffffff',
-          borderRadius: '25px',
-        },
-      }
-    );
-  }
-
-  const socket = io.connect('http://localhost:3002', {
-    transports: ['websocket'],
-    query: { userId }
-  });
-
-  socket.on('connect', () => {
-    console.log('Connected to WebSocket server');
-  });
-
-  socket.on('friendRequest', (data) => {
-    handleFriendNotification(data);
-  });
-
-  socket.on('acceptedRequest', (data) => {
-    handleFriendNotification(data);
-  })
-
-  socket.on('connect_error', (error) => {
-    console.error('Connection error:', error);
-  });
-
-  socket.on('disconnect', () => {
-    console.log('Disconnected from WebSocket server');
-  });
-  
   return (
     <div className='home-page'>
       <Navigator 
