aeae9a857b4b7376b57c96686dcd664851f89954
Hung Viet Hoang <hungdevngu99@gmail.com>
2023-10-13 20:12:38 +0700

    A01 Update Recommend Follow

--- src/components/BodyComponent/Body.js
+++ src/components/BodyComponent/Body.js
@@ -11,21 +11,56 @@ import { callGetApiWithoutToken } from '../../helpers/request';
 import './style/Body.scss'
 
 const apiDomain = process.env.REACT_APP_API_DOMAIN
+const LIMIT_RECOMMEN_SHOW = 3
+const PAGE_RECOMMEND = 1
 
 function Body(props) {
   const {typePage, setShowEditProfile} = props;
   const [postData, setPostData] = useState([]);
   const [userPaginationSelected, setUserPaginationSelected] = useState('home');
+  const [recommendUserData, setRecommendUserData] = useState(null)
 
   const getAllPost = async () => {
     const apiUrl = `${apiDomain}/v1/api/post/allPost`;
     const reponse = await callGetApiWithoutToken(apiUrl);
     setPostData(reponse.metaData.listPost);
   }
+
+  const isThereAnyRecommend = () => {
+    const visibleRecommendList = recommendUserData.RecommendFollowList.length
+    if(!visibleRecommendList)
+    {
+      return false
+    }
+    return visibleRecommendList > 0
+
+  }
+  const isThereAnyRecommendLeft = () => {
+    const totalRecommend = recommendUserData.totalRecommend
+    if(!totalRecommend )
+    {
+      return false
+    }
+    return isThereAnyRecommend() && LIMIT_RECOMMEN_SHOW < totalRecommend
+  }
   
+  const getRecommendFollowList = async (limit, page) => {
+    const apiUrl = `${apiDomain}/v1/api/user/recommendFollowing?limit=${limit}&page=${page}`;
+    const response = await callGetApiWithoutToken(apiUrl);
+    console.log(response)
+    setRecommendUserData(response.metaData);
+  }
+
+  const getNextRecomends = async (nextPage) =>
+  {
+    const response = await callGetApiWithoutToken(nextPage);
+    setRecommendUserData(response.metaData);
+  }
+
   useEffect(() => {
     if (typePage === 'HomePage') {
       getAllPost();
+      getRecommendFollowList(LIMIT_RECOMMEN_SHOW, PAGE_RECOMMEND);
     }
   }, []);
 
@@ -61,10 +96,18 @@ function Body(props) {
           <Fragment>
              <div className='group-component'>
               <div className='header-component title-text'>Who to Follow</div>
-              <RecommendFollower />
-              <RecommendFollower />
-              <RecommendFollower />
-              <div className='footer-component content-text'>See more suggestions</div>
+              {
+                recommendUserData != null && isThereAnyRecommend() > 0 &&
+                recommendUserData.RecommendFollowList.map((data, index) => {
+                  return (<RecommendFollower key={index} data={data} updateRecommendList = {() => {
+                    getRecommendFollowList(LIMIT_RECOMMEN_SHOW, PAGE_RECOMMEND)}}/>)
+                })
+              }
+              {
+                recommendUserData != null && isThereAnyRecommendLeft() &&
+                <div className='footer-component content-text' onClick={() => 
+                                      getNextRecomends(recommendUserData.nextPage)}>See more suggestions</div>
+              }
             </div>
             <div className='group-component'>
               <div className='header-component title-text'>Recently Save</div>
--- src/components/BodyComponent/RecommendFollower.js
+++ src/components/BodyComponent/RecommendFollower.js
@@ -1,21 +1,64 @@
-import React from 'react'
+import React, { useState, useEffect, Fragment } from 'react'
 import './style/RecommendFollower.scss';
+import { callPostApiWithoutToken } from '../../helpers/request';
+const apiDomain = process.env.REACT_APP_API_DOMAIN
 
-function RecommendFollower() {
+function RecommendFollower(props) {
+  const {data, updateRecommendList} = props;
+  const [recommendUser, setRecommendUser] = useState(null);
+
+  useEffect(() => {
+    handleRecommendUser(data)
+  }, [data]);
+
+  const handleRecommendUser = (data) => {
+    console.log(`set new userrecommend ${data}`)
+    let userAvatar = data.avatar || '/account-logo.png';
+    let userName = data.userName  || 'Ammonius';
+    let userId = data.userId
+    let bio = data.bio || "Nothing to descrip this person"
+    
+    let userInfo = {
+      userAvatar,
+      userName,
+      userId,
+      bio}
+    setRecommendUser(userInfo)
+  }
+
+  const makeRequestFollow = async () => {
+    console.log('following request')
+    if(!recommendUser)
+    {
+      console.error(`recommendUser does not exist`)
+      return
+    }
+    const apiUrl = `${apiDomain}/v1/api/user/friend_request/${recommendUser.userId}`
+    await callPostApiWithoutToken(apiUrl);
+    updateRecommendList()
+  }
+
   return (
     <div className='recomment-follower-component'>
-      <div className='image-follower' aria-hidden='true'>
-        <img src='/account-logo.png' alt='' />
-      </div>
-      <div className='information-follower'>
-        <div className='name-follower sub-title-text'>Radiant Earth Foundation</div>
-        <div className='summary-follower content-text'>Increasing shared understanding of our world ...</div>
-      </div>
-      <div className='add-follow-component'>
-        <div className='button-follow tag-text'>
-          Follow
-        </div>
-      </div>
+    {
+      recommendUser !== null &&
+      <Fragment>
+          <div className='image-follower' aria-hidden='true'>
+            <img src={recommendUser.userAvatar} alt='' />
+          </div>
+          <div className='information-follower'>
+            <div className='name-follower sub-title-text'>{recommendUser.userName}</div>
+            <div className='summary-follower content-text'>{recommendUser.bio}</div>
+          </div>
+          <div className='add-follow-component'>
+            <div className='button-follow tag-text' onClick={() => {
+              makeRequestFollow();
+            }}>
+              Follow
+            </div>
+          </div>
+      </Fragment>
+    }
     </div>
   )
 }
